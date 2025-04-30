5a67f89b4ad2d62bfcbfd12e2df45333898df323
Ankit-84 <ankitkumar43975@gmail.com>
2025-04-30 21:57:47 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.use(express.json({ limit: "10mb" })); // Set a limit of 10 MB for JSON payloads
+app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-// app.use(express.json());
+
 app.use(cookieParser());
 app.use(
   cors({
@@ -31,13 +31,6 @@ app.get("/",(req,res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -9,7 +9,6 @@ const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173","https://chatting-app-frontend-hd52.onrender.com"],
   },
-  credentials: true
 });
 
 export function getReceiverSocketId(userId) {
