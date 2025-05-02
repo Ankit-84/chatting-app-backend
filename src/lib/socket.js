f6db3b5fba5e0ff00606ed10c9024f108b41e4e8
Ankit-84 <ankitkumar43975@gmail.com>
2025-05-02 20:34:51 +0530

    frontend

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chatting-app-frontend-hd52.onrender.com"],
+    origin: ["http://localhost:5173","https://chatting-app-5gji.onrender.com"],
   },
 });
 
